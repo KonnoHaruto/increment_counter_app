dee02ddae51d51a9182761d0a3d38388443f03f2
Haruto Konno <qingdoujinye@gmail.com>
2021-08-29 21:06:27 +0900

    内容を修正

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
   }
 }
