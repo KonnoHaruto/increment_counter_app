07f6dd357736f15df0cca523303b35b954edc098
Haruto Konno <qingdoujinye@gmail.com>
2021-08-30 00:02:04 +0900

    パッケージを修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'view/home_page.dart';
+
 void main() {
   runApp(
     const ProviderScope(
@@ -21,14 +23,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
