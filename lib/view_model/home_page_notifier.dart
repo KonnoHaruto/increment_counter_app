a8acad0d5ad6caaadd25d2a755193c628e144758
Haruto Konno <qingdoujinye@gmail.com>
2021-08-30 15:19:17 +0900

    メソッドを定義

--- lib/view_model/home_page_notifier.dart
+++ lib/view_model/home_page_notifier.dart
@@ -1,3 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:increment_counter_app/model/home_page_state.dart';
 
-final myProvider = Provider<String>((_) => 'Japan');
+//Providerの定数をグローバルに宣言
+//StateNotifierProviderに続けてNotifierクラスの型と、格納する状態の型を明示する
+final counterProvider =
+    //autoDispose修飾子でProviderを破棄
+    StateNotifierProvider.autoDispose<HomePageNotifier, Counter>(
+  (ref) => HomePageNotifier(),
+);
+
+class HomePageNotifier extends StateNotifier<Counter> {
+  //コンストラクター
+  HomePageNotifier() : super(const Counter());
+
+  //カウンターををインクリメント
+  void increment() {
+    state = state.copyWith(counter: state.counter + 1);
+  }
+}
