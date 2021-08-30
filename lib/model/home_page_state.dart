1f493e8bb34884e6737a155caa90c66daf2a812b
Haruto Konno <qingdoujinye@gmail.com>
2021-08-30 13:47:03 +0900

    修正

--- lib/model/home_page_state.dart
+++ lib/model/home_page_state.dart
@@ -1,11 +1,1 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'home_page_state.freezed.dart';
-
-@freezed
-class HomePageState with _$HomePageState {
-  const factory HomePageState({
-    @Default(0) int mainCount,
-    @Default(0) int subCount,
-  }) = _HomePageState;
-}
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,35 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'home_page_notifier.dart';
-
-class HomePage extends ConsumerWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // HomePageState
-    final pageState = ref.watch(homePageNotifierProvider);
-    // HomePageNotifier
-    final pageNotifier = ref.watch(homePageNotifierProvider.notifier);
-
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: pageNotifier.resetAllCount,
-        child: Icon(Icons.exposure_zero),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('MainCount ${pageState.mainCount}'),
-            onTap: pageNotifier.increaseMainCount,
-          ),
-          ListTile(
-            title: Text('SubCount ${pageState.subCount}'),
-            onTap: pageNotifier.increaseSubCount,
-          ),
-        ],
-      ),
-    );
+  Widget build(BuildContext context) {
+    return const Scaffold();
   }
 }
