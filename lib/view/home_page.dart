77ae18f8486634babb7a0c96b8fcd6775f2c0c4b
Haruto Konno <qingdoujinye@gmail.com>
2021-08-30 15:31:39 +0900

    viewを作成

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:increment_counter_app/view_model/home_page_notifier.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+  Widget build(BuildContext context, ScopedReader watch) {
+    //更新
+    final counter = watch(counterProvider);
+    //Providerを読み取る
+    final notifier = watch(counterProvider.notifier);
+    return const Scaffold(
+        //TODO make UI
+        );
   }
 }
